403900312b49a10d375e9b0c3e1ff11a2cb7da5d
Masita <masita.mohamad@gmail.com>
2020-06-26 14:24:46 -0500

    Updated README

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Welcome to the newsroom! You've just accepted a data visualization position for 
 
 The editor wants to run a series of feature stories about the health risks facing particular demographics. She's counting on you to sniff out the first story idea by sifting through information from the U.S. Census Bureau and the Behavioral Risk Factor Surveillance System.
 
-The data set included with the assignment is based on 2014 ACS 1-year estimates: [https://factfinder.census.gov/faces/nav/jsf/pages/searchresults.xhtml](https://factfinder.census.gov/faces/nav/jsf/pages/searchresults.xhtml), but you are free to investigate a different data set. The current data set incldes data on rates of income, obesity, poverty, etc. by state. MOE stands for "margin of error."
+The data set included with the assignment is based on 2014 ACS 1-year estimates: [https://www.census.gov/programs-surveys/acs/technical-documentation/table-and-geography-changes/2014/1-year.html](https://www.census.gov/programs-surveys/acs/technical-documentation/table-and-geography-changes/2014/1-year.html), but you are free to investigate a different data set. The current data set incldes data on rates of income, obesity, poverty, etc. by state. MOE stands for "margin of error."
 
 ## Your Task
 
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,66 @@
-// @TODO: YOUR CODE HERE!
+
+
+var svgWidth = 900;
+var svgHeight = 600;
+
+var margin = 50;
+
+var height = svgHeight - margin - margin;
+var width = svgWidth - margin - margin;
+
+// data
+var pizzasEatenByMonth = [15, 5, 25, 18, 12, 22, 0, 4, 15, 10, 21, 2];
+
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth)
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin}, ${margin})`)
+
+var xScale = d3.scaleLinear()
+  .domain([0, pizzasEatenByMonth.length])
+  .range([0, width])
+
+var yScale = d3.scaleLinear()
+  .domain([0, d3.max(pizzasEatenByMonth)])
+  .range([height, 0])
+
+var line = d3.line()
+  .x((d, i) => xScale(i))  
+  .y(d => yScale(d))
+
+chartGroup.append("path")
+  .attr("d", line(pizzasEatenByMonth))
+  .attr("fill", "none")
+  .attr("stroke", "green");
+
+var circlesGroup = chartGroup.selectAll("circle")
+  .data(pizzasEatenByMonth)
+  .enter()
+  .append("circle")
+  .attr("cx", line)
+  .attr("r", 5)
+  .attr("fill", "red");
+
+circlesGroup
+  .transition()
+  .duration(2000)
+  .attr("cx", (d,i) => xScale(i))
+  .attr("cy", d => yScale(d));
+
+
+// Event listeners with transitions
+circlesGroup.on("mouseover", function() {
+  d3.select(this)
+    .transition()
+    .duration(500)
+    .attr("r", 20)
+    .attr("fill", "lightblue")
+}).on("mouseout", function() {
+  d3.select(this)
+    .transition()
+    .duration(1000)
+    .attr("r", 5)
+})
